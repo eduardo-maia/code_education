59185acced40d1852672d953808449778099447a
Eduardo Maia <maia@eduardomaia.com>
2015-02-12 15:43:05 -0200

    Usando file_exists como double check se arquivo existe
    Signed-off-by: Eduardo Maia <maia@eduardomaia.com>

--- index.php
+++ index.php
@@ -12,9 +12,8 @@ if ($path == "")
 	}
 
 # Optei por registrar as páginas em array, para não permitir acesso a arquivos que não quero que sejam acessados
-# Outra opção seria utilizar bool file_exists ( string $filename )
-$allowed_pages = array ("home"=>1,"empresa"=>1,"produtos"=>1,"servicos"=>1,"contato"=>1);  
-if ( !isset($allowed_pages[$path]) )
+$allowed_pages = array ("home"=>1,"empresa"=>1,"produtos"=>1,"servicos"=>1,"contato"=>1);
+if ( !isset($allowed_pages[$path]) || !file_exists($path.".php") )
 	{
 	http_response_code(404);
 	echo "P&aacute;gina $page n&atilde;o encontrada.";
@@ -37,21 +36,21 @@ $page = verifica_rota();
 	</head>
 
 	<body>
-	
+
 	<script src="js/jquery-2.1.3.min.js"></script>
 
 	<div class="navbar navbar-inverse navbar-static-top">
-	
+
 		<div class="container">
-			
+
 			<a href="#" class="navbar-brand">PHP + bootstrap</a>
-			
+
 			<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			
+
 			<div class="collapse navbar-collapse navHeaderCollapse">
 				<ul class="nav navbar-nav navbar-right">
 					<li <?php if ($page=='home') echo 'class="active"';?>><a href="/">Home</a></li>
@@ -60,15 +59,15 @@ $page = verifica_rota();
 					<li <?php if ($page=='servicos') echo 'class="active"';?>><a href="/servicos">Servi&ccedil;os</a></li>
 					<li <?php if ($page=='contato') echo 'class="active"';?>><a href="/contato">Contato</a></li>
 				</ul>
-					
+
 			</div>
-			
+
 		</div>
-	
+
 	</div>
-	
-	
-	
+
+
+
 	<!-- HERE COMES THE PAGE CONTENT -->
 	<div class="container">
 		<div class="jumbotron">
@@ -80,13 +79,13 @@ $page = verifica_rota();
 		</div>
 	</div>
 	<!-- END PAGE CONTENT -->
-	
-	
+
+
 	<script src="js/bootstrap.min.js"></script>
 
 	<div class="navbar navbar-default navbar-fixed-bottom">
 		<div class="container">
-			<p class="navbar-text pull-left">Todos os direitos reservados - 
+			<p class="navbar-text pull-left">Todos os direitos reservados -
 				<?php
 				date_default_timezone_set("America/Sao_Paulo");
 				echo date("Y");
